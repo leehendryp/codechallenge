d6c78fd927a35bc4025db3b4827714837735d2a4
leehendryp <leehendryp@gmail.com>
2025-03-19 11:54:29 +0000

    feat(injection): set up base Hilt configuration

---
+++ app/src/main/java/com/leehendryp/codechallenge/core/CodeChallengeApp.kt
@@ -0,0 +1,7 @@
+package com.leehendryp.codechallenge.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+internal class CodeChallengeApp : Application()
