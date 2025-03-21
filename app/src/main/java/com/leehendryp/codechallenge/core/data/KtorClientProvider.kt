3e07c9be6661b9c3d0385055e58302b511f426c0
leehendryp <leehendryp@gmail.com>
2025-03-21 11:26:31 +0000

    refactor(networking): inject Ktor HTTP engine

---
+++ app/src/main/java/com/leehendryp/codechallenge/core/data/KtorClientProvider.kt
@@ -0,0 +1,54 @@
+package com.leehendryp.codechallenge.core.data
+
+import com.leehendryp.codechallenge.BuildConfig
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.ANDROID
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
+
+private const val TIME_OUT = 10_000L
+
+internal class KtorClientProvider @Inject constructor(
+    engine: HttpClientEngine,
+) {
+    val client = HttpClient(engine) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                },
+            )
+        }
+
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = BuildConfig.API_HOST
+            }
+        }
+
+        install(Logging) {
+            logger = Logger.ANDROID
+            level = LogLevel.ALL
+            sanitizeHeader { header -> header == HttpHeaders.Authorization }
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = TIME_OUT
+            connectTimeoutMillis = TIME_OUT
+            socketTimeoutMillis = TIME_OUT
+        }
+    }
+}
