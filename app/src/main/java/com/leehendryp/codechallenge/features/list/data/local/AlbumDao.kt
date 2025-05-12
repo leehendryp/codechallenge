39d74fc4e092a7ecd342d7ae85c7d1bdbac745eb
leehendryp <leehendryp@gmail.com>
2025-05-12 22:51:19 +0100

    feat(persistence): add Paging3 to DAO

--- app/src/main/java/com/leehendryp/codechallenge/features/list/data/local/AlbumDao.kt
+++ app/src/main/java/com/leehendryp/codechallenge/features/list/data/local/AlbumDao.kt
@@ -1,16 +1,16 @@
 package com.leehendryp.codechallenge.features.list.data.local
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.leehendryp.codechallenge.features.list.data.local.model.AlbumEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AlbumDao {
-    @Query("SELECT * FROM album")
-    fun getAll(): Flow<List<AlbumEntity>>
+    @Query("SELECT * FROM album ORDER BY title ASC")
+    fun getPagedAlbums(): PagingSource<Int, AlbumEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(albums: List<AlbumEntity>)
