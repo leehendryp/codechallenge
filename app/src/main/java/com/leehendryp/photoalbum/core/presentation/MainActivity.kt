098ecfe6085b96e50762261b1683932cfc300e81
leehendryp <leehendryp@gmail.com>
2025-05-26 11:20:27 +0100

    feat(app): add back icon to top bar

--- app/src/main/java/com/leehendryp/photoalbum/core/presentation/MainActivity.kt
+++ app/src/main/java/com/leehendryp/photoalbum/core/presentation/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.Crossfade
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -23,6 +30,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.leehendryp.photoalbum.R
 import com.leehendryp.photoalbum.core.presentation.navigation.AppNavGraph
 import com.leehendryp.photoalbum.core.presentation.navigation.Destination
 import com.leehendryp.photoalbum.core.presentation.navigation.screens
@@ -56,11 +64,16 @@ internal class MainActivity : ComponentActivity() {
 
         Scaffold(
             topBar = {
-                Crossfade(currentScreen) { currentScreen ->
-                    TopAppBar(
-                        title = { TopBarTitle(currentScreen) },
-                    )
-                }
+                TopAppBar(
+                    title = { TopBarTitle(currentScreen) },
+                    navigationIcon = {
+                        if (navController.previousBackStackEntry != null) {
+                            BackIconButton(isVisible = currentScreen != AlbumList) {
+                                navController.navigateUp()
+                            }
+                        }
+                    },
+                )
             },
             snackbarHost = { SnackbarHost(snackbarHostState) },
         ) { innerPadding ->
@@ -69,6 +82,22 @@ internal class MainActivity : ComponentActivity() {
                 snackbarHostState = snackbarHostState,
                 modifier = Modifier.padding(innerPadding),
             )
+        }
+    }
+
+    @Composable
+    private fun BackIconButton(isVisible: Boolean, onClick: () -> Unit) {
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn() + slideInHorizontally(),
+            exit = fadeOut() + slideOutHorizontally(),
+        ) {
+            IconButton(onClick = onClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.top_bar_back_button_content_description),
+                )
+            }
         }
     }
 
